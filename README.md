0c68522949b9c1aeb62c06102f9099a2b51b1758
Andrew Aubury <andrewaubury1@gmail.com>
2018-03-08 21:54:02 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This Package is just an implementaion of the IPN class of this Repository
+
+https://github.com/Quixotix/PHP-PayPal-IPN
+
---
+++ src/PaypalIPNServiceProvider.php
@@ -0,0 +1,27 @@
+<?php namespace Andrew\PaypalIPN;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaypalIPNServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->make('Andrew\PaypalIPN\PaypalIPNListener');
+	}
+
+}
